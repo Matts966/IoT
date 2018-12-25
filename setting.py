2d1d4919f6f9208c4cda2994003b291c2808b287
Matts966 <matsui0415@decoo.co.jp>
2018-12-25 19:24:47 +0900

    debug

--- setting.py
+++ setting.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 from dotenv import Dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
-Dotenv(dotenv_path)
+dotenv = Dotenv(dotenv_path)
 os.environ.update(dotenv)
 
 URL = os.environ.get("URL")
