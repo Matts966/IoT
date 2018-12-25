8629a01e9860e268f8c9288077ead7be9e8cd682
Matts966 <matsui0415@decoo.co.jp>
2018-12-25 18:21:38 +0900

    add light control system.

---
+++ control_light.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import setting.py
+
+ucode = "a304-4"
+url = setting.URL
+headers = {"Content-Type": "application/json"}
+auth = setting.AUTH
+
+putdata_on = {
+    'ucode': ucode, # Name or ucode
+    'instance': "on"
+}
+putdata_off = {
+    'ucode': ucode,
+    'instance': "off"
+}
+
+def _put(put_data):
+    req = requests.put(url, data=json.dumps(
+        putdata), headers=headers, auth=auth)
+    return req.status_code
+
+def light_on():
+    return _put(putdata_on)
+
+def light_off():
+    return _put(putdata_off)
+
